1f291dcacea100829ef6d57b9104870a079cbe41
João Octávio <101433733+Jaoctavio@users.noreply.github.com>
2022-09-30 17:13:35 -0300

    Add files via upload

---
+++ navChange.js
@@ -0,0 +1,36 @@
+function changeBody(page) {
+  fetch(page)
+    .then((res) => res.text())
+    .then((res) => {
+      const isPokedexBody = page === "zextern.html" ? true : false;
+      document.body.innerHTML = res;
+
+      if (isPokedexBody) {
+        getPokemonData();
+      }
+    });
+}
+
+function dark_mode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
